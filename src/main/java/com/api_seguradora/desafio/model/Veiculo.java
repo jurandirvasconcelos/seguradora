62aea496ab9a8e77a63496fcad88e4d63ed45550
jurandirvasconcelos <jurandir.vasconcelos2@gmail.com>
2023-06-12 18:06:56 -0300

    create entity Veiculo

---
+++ src/main/java/com/api_seguradora/desafio/model/Veiculo.java
@@ -0,0 +1,23 @@
+package com.api_seguradora.desafio.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "veiculos")
+public class Veiculo {
+
+    private String id;
+
+    private String marca;
+
+    private String modelo;
+
+    private String placa;
+    
+}
