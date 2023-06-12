f409ad2c780a6dbc79abc388f31cee52309f9cb7
jurandirvasconcelos <jurandir.vasconcelos2@gmail.com>
2023-06-12 17:16:37 -0300

    test method listService of class Seguradoservice

---
+++ src/test/java/com/api_seguradora/desafio/model/segurado/SeguradoTest.java
@@ -0,0 +1,47 @@
+package com.api_seguradora.desafio.model.segurado;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.api_seguradora.desafio.database.repository.SeguradoRepository;
+import com.api_seguradora.desafio.model.Segurado;
+import com.api_seguradora.desafio.service.SeguradoService;
+
+public class SeguradoTest {
+    
+    @Mock
+    private SeguradoRepository seguradoRepository;
+
+    @InjectMocks
+    private SeguradoService seguradoService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void shouldShowAllSegurados() {
+        List<Segurado> segurados = new ArrayList<>();
+        segurados.add(new Segurado("1", "John Doe", "12345678900"));
+        segurados.add(new Segurado("2", "Jane Smith", "98765432100"));
+
+        when(seguradoRepository.findAll()).thenReturn(segurados);
+
+        List<Segurado> resultado = seguradoService.listSegurados();
+
+        assertEquals(2, resultado.size());
+        verify(seguradoRepository, times(1)).findAll();
+}  
+}
