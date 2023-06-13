1eb618a8523bd8c0d857a9dd419e424fdc4c4c8b
jurandirvasconcelos <jurandir.vasconcelos2@gmail.com>
2023-06-13 14:44:11 -0300

    created shouldShowSeguradoById test

--- src/test/java/com/api_seguradora/desafio/model/SeguradoTest.java
+++ src/test/java/com/api_seguradora/desafio/model/SeguradoTest.java
@@ -1,12 +1,14 @@
 package com.api_seguradora.desafio.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -42,5 +44,20 @@ public class SeguradoTest {
 
         assertEquals(2, resultado.size());
         verify(seguradoRepository, times(1)).findAll();
+    }
+
+    @Test
+    void shouldShowSeguradoById() {
+        String id = "1";
+        Segurado segurado = new Segurado();
+        segurado.setId(id);
+
+        when(seguradoRepository.findById(id)).thenReturn(Optional.of(segurado));
+
+        Segurado resultado = seguradoService.getSeguradoById(id);
+
+        assertNotNull(resultado);
+        assertEquals(id, resultado.getId());
+        verify(seguradoRepository, times(1)).findById(id);
     }
 }
