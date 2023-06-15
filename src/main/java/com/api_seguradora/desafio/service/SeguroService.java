391e6506feac00228ff12e70bda321399683abdf
jurandirvasconcelos <jurandir.vasconcelos2@gmail.com>
2023-06-15 12:13:54 -0300

    refactor SegurService

--- src/main/java/com/api_seguradora/desafio/service/SeguroService.java
+++ src/main/java/com/api_seguradora/desafio/service/SeguroService.java
@@ -1,65 +1,89 @@
 package com.api_seguradora.desafio.service;
 
-import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.api_seguradora.desafio.database.repository.SeguradoRepository;
+import com.api_seguradora.desafio.database.dto.SeguradoDTO;
+import com.api_seguradora.desafio.database.dto.SeguroDTO;
+import com.api_seguradora.desafio.database.dto.VeiculoDTO;
 import com.api_seguradora.desafio.database.repository.SeguroRepository;
-import com.api_seguradora.desafio.database.repository.VeiculoRepository;
 import com.api_seguradora.desafio.model.Segurado;
 import com.api_seguradora.desafio.model.Seguro;
 import com.api_seguradora.desafio.model.Veiculo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SeguroService {
 
     private final SeguroRepository seguroRepository;
-    private final SeguradoRepository seguradoRepository;
-    private final VeiculoRepository veiculoRepository;
+    private final SeguradoService seguradoService;
+    private final VeiculoService veiculoService;
 
-    public SeguroService(SeguroRepository seguroRepository, SeguradoRepository seguradoRepository, VeiculoRepository veiculoRepository) {
+    @Autowired
+    public SeguroService(SeguroRepository seguroRepository, SeguradoService seguradoService,
+            VeiculoService veiculoService) {
         this.seguroRepository = seguroRepository;
-        this.seguradoRepository = seguradoRepository;
-        this.veiculoRepository = veiculoRepository;
+        this.seguradoService = seguradoService;
+        this.veiculoService = veiculoService;
     }
 
-    public Seguro getSeguroById(String id) {
+    public List<SeguroDTO> listarSeguros() {
+        List<Seguro> seguros = seguroRepository.findAll();
+        List<SeguroDTO> segurosDTO = new ArrayList<>();
+
+        for (Seguro seguro : seguros) {
+            SeguradoDTO seguradoDTO = seguradoService.obterSeguradoDTO(seguro.getSeguradoId());
+            VeiculoDTO veiculoDTO = veiculoService.obterVeiculoDTO(seguro.getVeiculoId());
+
+            SeguroDTO seguroDTO = new SeguroDTO(seguro, seguradoDTO, veiculoDTO);
+            segurosDTO.add(seguroDTO);
+        }
+
+        return segurosDTO;
+    }
+
+    public SeguroDTO cadastrarSeguro(SeguroDTO seguroDTO) {
+        Seguro seguro = new Seguro();
+        seguro.setPreco(seguroDTO.getPreco());
+        seguro.setDataAdesao(seguroDTO.getDataAdesao());
+
+        // Obter o Segurado pelo ID
+        SeguradoDTO seguradoDTO = seguradoService.obterSeguradoDTO(seguroDTO.getSeguradoId());
+        Segurado segurado = new Segurado();
+        segurado.setId(seguradoDTO.getId());
+        segurado.setName(seguradoDTO.getName());
+        segurado.setCpf(seguradoDTO.getCpf());
+        seguro.setSegurado(segurado);
+
+        // Obter o Veículo pelo ID
+        VeiculoDTO veiculoDTO = veiculoService.obterVeiculoDTO(seguroDTO.getVeiculoId());
+        Veiculo veiculo = new Veiculo();
+        veiculo.setId(veiculoDTO.getId());
+        veiculo.setMarca(veiculoDTO.getMarca());
+        veiculo.setModelo(veiculoDTO.getModelo());
+        veiculo.setPlaca(veiculoDTO.getPlaca());
+        seguro.setVeiculo(veiculo);
+
+        Seguro seguroSalvo = seguroRepository.save(seguro);
+
+        return new SeguroDTO(seguroSalvo, seguradoDTO, veiculoDTO);
+    }
+
+    public Seguro obterSeguroPorId(String id) {
         return seguroRepository.findById(id).orElse(null);
     }
 
-    public List<Seguro> listSeguros(){
-        return seguroRepository.findAll();
-    }
-
-    public Seguro saveSeguro(Seguro seguro) {
-        Segurado segurado = seguradoRepository.findById(seguro.getSeguradoId()).orElse(null);
-        Veiculo veiculo = veiculoRepository.findById(seguro.getVeiculoId()).orElse(null);
-
-        if (segurado != null && veiculo != null) {
-            seguro.setSegurado(segurado);
-            seguro.setVeiculo(veiculo);
+    public Seguro atualizarSeguro(String id, Seguro seguro) {
+        if (seguroRepository.existsById(id)) {
+            seguro.setId(id);
             return seguroRepository.save(seguro);
         }
         return null;
     }
 
-    public Seguro updateSeguro(String id, Seguro seguro) {
-        if (seguroRepository.existsById(id)) {
-            Segurado segurado = seguradoRepository.findById(seguro.getSeguradoId()).orElse(null);
-            Veiculo veiculo = veiculoRepository.findById(seguro.getVeiculoId()).orElse(null);
-
-            if (segurado != null && veiculo != null) {
-                seguro.setId(id);
-                seguro.setSegurado(segurado);
-                seguro.setVeiculo(veiculo);
-                return seguroRepository.save(seguro);
-            }
-        }
-        return null;
-    }
-
-    public boolean deleteSeguro(String id) {
+    public boolean deletarSeguro(String id) {
         if (seguroRepository.existsById(id)) {
             seguroRepository.deleteById(id);
             return true;
