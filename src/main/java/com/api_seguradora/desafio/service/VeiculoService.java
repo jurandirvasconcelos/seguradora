fcf854f3f730792cf206c240a2f1a1dfa4c35f1c
jurandirvasconcelos <jurandir.vasconcelos2@gmail.com>
2023-06-15 00:31:30 -0300

    add saveVeiculo, updateVeiculo and deleteVeiculo

--- src/main/java/com/api_seguradora/desafio/service/VeiculoService.java
+++ src/main/java/com/api_seguradora/desafio/service/VeiculoService.java
@@ -2,9 +2,12 @@ package com.api_seguradora.desafio.service;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
 import com.api_seguradora.desafio.database.repository.VeiculoRepository;
 import com.api_seguradora.desafio.model.Veiculo;
 
+@Service
 public class VeiculoService {
 
     private VeiculoRepository veiculoRepository;
@@ -15,5 +18,25 @@ public class VeiculoService {
 
     public Veiculo getVeiculoById(String id) {
         return veiculoRepository.findById(id).orElse(null);
+    }
+
+    public Veiculo saveVeiculo(Veiculo veiculo) {
+        return veiculoRepository.save(veiculo);
+    }
+
+    public Veiculo updateVeiculo(String id, Veiculo veiculo) {
+        if (veiculoRepository.existsById(id)) {
+            veiculo.setId(id);
+            return veiculoRepository.save(veiculo);
+        }
+        return null;
+    }
+
+    public boolean deleteVeiculo(String id) {
+        if (veiculoRepository.existsById(id)) {
+            veiculoRepository.deleteById(id);
+            return true;
+        }
+        return false;
     }
 }
